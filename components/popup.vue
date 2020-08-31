d40c6338301b7b423c9ca301af6e1cef840607c3
svoyparen <62475107+svoyparen@users.noreply.github.com>
2020-09-01 00:04:15 +0300

    Add files via upload

---
+++ components/popup.vue
@@ -0,0 +1,101 @@
+<template>
+	<div class="modal-mask" v-if="this.isVisible">
+		<div class="modal-container">
+			<form class="form">
+				<label for="title">Название статьи</label><br />
+				<input type="text" name="title" v-model.trim="title"><br />
+				<label for="text">Текст статьи</label><br />
+				<textarea name="text">{{ text }}</textarea><br />
+				<span class="save" @click="save">Сохранить</span>
+				<span class="reset" @click="reset">Сбросить</span>
+			</form>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: {
+			id: { type: Number },
+			title: { type: String, default: '' },
+			text: { type: String, default: '' },
+		},
+
+		data: () => ({
+			isVisible: false
+		}),
+
+		methods: {
+			setVisible(flag) {
+				console.log('Flag is ' + flag)
+				this.isVisible = flag
+			},
+
+			setData(item) {
+				this.title = item.title
+				this.text = item.text
+			},
+
+			save() {
+				console.log('save')
+			},
+
+			reset() {
+				console.log('reset')
+			}
+
+		}
+	}
+</script>
+
+<style scoped>
+	.modal-mask {
+		position: fixed;
+		z-index: 9998;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background-color: rgba(0, 0, 0, 0.7);
+		display: table;
+		transition: opacity 0.9s ease;
+	}
+	.modal-container {
+		max-width: 600px;
+		margin: 50px auto;
+		background-color: #fff;
+		border-radius: 10px;
+		box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+		transition: all 0.3s ease;
+		font-family: Helvetica, Arial, sans-serif;
+	}
+	.form {
+		width: 90%;
+		padding: 15px;
+		margin: 15px;
+	}
+	input {
+		width: 100%;
+		margin: 15px;
+		padding: 15px;
+	}
+	textarea {
+		width: 100%;
+		margin: 15px;
+		padding: 15px;
+
+	}
+	.save {
+		margin: 15px;
+		background-color: #00cc00;
+		color: white;
+		padding: 5px 15px;
+	}
+	.reset {
+		margin: 15px;
+		background-color: #cc3333;
+		color: white;
+		padding: 5px 15px;
+
+	}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="container">
+    <Table
+      :items="items"
+      @showPopup="openPopup"
+      />
+      <Popup
+        ref="popup"
+      />
+  </div>
+</template>
+
+<script>
+  import Table from '~/components/table'
+  import Popup from '~/components/popup'
+  import axios from 'axios'
+
+  export default {
+    components: {
+      Table,
+      Popup,
+    },
+
+    data: () => ({
+      items: [],
+    }),
+
+    mounted() {
+      this.getData()
+    },
+
+    methods: {
+      getData(args = []) {
+        axios.get('https://test.cornapi.ru/blog')
+        .then( response => {
+          for ( const item in response.data.data.items ){
+            console.log( response.data.data.items[item] )
+            this.items.push(response.data.data.items[item])
+          }
+        })
+      },
+
+      openPopup(item) {
+        this.$refs.popup.setVisible(true)
+        this.$refs.popup.setData(item)
+      }
+
+    },
+
+    }
+
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
