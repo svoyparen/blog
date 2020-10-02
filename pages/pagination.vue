a0f39dc3615092a907cf9adaddfb7af7289f3827
svoyparen <62475107+svoyparen@users.noreply.github.com>
2020-10-02 21:20:13 +0300

    Add files via upload

--- pages/pagination.vue
+++ pages/pagination.vue
@@ -24,51 +24,45 @@
   import Popup from '@/components/popup'
   import PushMessage from '~/components/PushMessage'
   import axios from 'axios'
-
   export default {
     components: {
       Table2,
       Popup,
       PushMessage,
     },
-
     data: () => ({
       items: [],
       loading: false,
       nextRow: null,
       quantityPages: 1,
       messageFlag: true,
+      page: 1,
     }),
 
     mounted() {
-		  this.getData()
+      this.getData()
     },
 
     methods: {
-
-    	async getData(page = 1) {
-				let response = await axios.get('https://test.cornapi.ru/blog/paginate', {params: { 'page': page }} )
-	        	.catch( error => {
-	        		console.log('Smthng wrong! ' + e)
-	        	})
+      async getData(page = this.page) {
+        this.page = page
+        let response = await axios.get('https://test.cornapi.ru/blog/paginate', {params: { 'page': page }} )
+            .catch( error => {
+              console.log('Smthng wrong! ' + e)
+            })
         this.items = []
-      	response.data.data.items.forEach((item) => this.items.push(item))
-      	this.nextRow = response.data.data.nextRow
+        response.data.data.items.forEach((item) => this.items.push(item))
+        this.nextRow = response.data.data.nextRow
         this.quantityPages = response.data.data.quantityPages
-
-		},
-	
+    },
+  
       openPopup(item) {
         this.$refs.popup.setVisible(true)
         this.$refs.popup.setData(item)
       },
-
-      showPushMessage(message) {
-        this.$refs.message.setVisible(message)
+      showPushMessage(data) {
+        this.$refs.message.setVisible(data['message'], data['type'])
       },
     },
-
   }
-
 </script>
-
